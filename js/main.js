a2a0eb1d2602767260066c34c54d2fcbc31876c3
kamisamatenshi <tsecm234@gmail.com>
2018-07-26 10:08:54 +0800

    A52
    A52

--- js/main.js
+++ js/main.js
@@ -80,5 +80,28 @@ function myFunction() {
        });
    }
     setTimeout(fl,4000);
-  
+  /////////////////////////////////////////////////
+const app = new Vue({
+  el:'#app',
+  data: {
+    name: '',
+    age: 0
+  },
+  mounted() {
+    if (localStorage.name) {
+      this.name = localStorage.name;
+    }
+    if (localStorage.age) {
+      this.age = localStorage.age;
+    }
+  },
+  methods: {
+    persist() {
+      localStorage.name = this.name;
+      localStorage.age = this.age;
+      console.log('now pretend I did more stuff...');
+    }
+  }
+})
+    
 });
